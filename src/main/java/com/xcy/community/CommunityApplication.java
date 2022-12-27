6c4f642057e8fb07f3054a13d08641c7d710cdbd
huluhuluaaa <626030651@qq.com>
2022-12-27 13:46:46 +0800

    none

--- src/main/java/com/xcy/community/CommunityApplication.java
+++ src/main/java/com/xcy/community/CommunityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommunityApplication {
 
     public static void main(String[] args) {
-        //<_..._>
+        //<something>
         SpringApplication.run(CommunityApplication.class, args);
     }
 
