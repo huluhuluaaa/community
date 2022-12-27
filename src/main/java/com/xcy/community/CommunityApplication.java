3c7f5a0a50066b1c35c119585048377eb9eafa2a
huluhuluaaa <78630462+huluhuluaaa@users.noreply.github.com>
2022-12-27 13:23:12 +0800

    Update CommunityApplication.java

--- src/main/java/com/xcy/community/CommunityApplication.java
+++ src/main/java/com/xcy/community/CommunityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommunityApplication {
 
     public static void main(String[] args) {
-        //for push
+        //<_._>
         SpringApplication.run(CommunityApplication.class, args);
     }
 
